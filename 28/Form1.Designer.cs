b5859a3922da2a95d4e747ca0214997afdf9ab12
alecs000 <alecs.2004fed@gmail.com>
2021-10-20 22:32:37 +0300

    Добавьте файлы проекта.

---
+++ 28/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿
+namespace _28
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Controls.Add(this.pictureBox2);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(800, 450);
+            this.panel1.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.pictureBox1.Location = new System.Drawing.Point(595, 301);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(25, 25);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.Red;
+            this.pictureBox2.Location = new System.Drawing.Point(361, 138);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(25, 25);
+            this.pictureBox2.TabIndex = 0;
+            this.pictureBox2.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ 28/Form1.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _28
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.KeyDown += new KeyEventHandler(Form1_KeyDown);
+            timer1.Start();
+            timer1.Interval = 150;
+        }
+        public int loc;
+        public int sch=0;
+        void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch(e.KeyCode.ToString())
+            {
+                case "Right":
+                    loc = 1;
+                    break;
+                case "Left":
+                    loc = 2;
+                    break;
+                case "Up":
+                    loc = 3;
+                    break;
+                case "Down":
+                    loc = 4;
+                    break;
+
+            }
+        }
+        int x = 0;
+        int y = 0;
+        int x1 = 0;
+        int y1 = 0;
+        int fx = 0;
+        int fy = 0;
+        PictureBox[] snake = new PictureBox[400];
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (pictureBox1.Location.X>725|| pictureBox1.Location.X < 0 || pictureBox1.Location.Y > 425 | pictureBox1.Location.X < 0 )
+            {
+                throw new Exception("GTAME OVER!");
+            }
+            if (pictureBox2.Location.X< pictureBox1.Location.X+15 && pictureBox2.Location.X > pictureBox1.Location.X - 15&& pictureBox2.Location.Y < pictureBox1.Location.Y + 15 && pictureBox2.Location.Y > pictureBox1.Location.Y - 15)
+            {
+                int p1 = panel1.Size.Width;
+                int p2 = panel1.Size.Height;
+                int s1 = pictureBox2.Size.Width;
+                int s2 = pictureBox2.Size.Height;
+                
+
+                Random rnd = new Random();
+                pictureBox2.Location = new Point(rnd.Next(p1 - s1), rnd.Next(p2 - s2));
+
+                pictureBox2.Visible = true;
+                PictureBox pct = new PictureBox();
+                pct.Height = 25;
+                pct.Width = 25;
+                pct.Name = "pct" + sch;
+                pct.Location = new Point(pictureBox1.Location.X, pictureBox1.Location.Y);
+                pct.BackColor = Color.Aqua;
+                panel1.Controls.Add(pct);
+                snake[sch] = pct;
+                sch++;
+
+            }
+            for (int i = 0; i < sch; i++)
+            {
+
+                
+                if (i != 0)
+                {
+                    snake[i].Location = new Point(x1, y1);
+                }
+
+
+
+
+                x1 = snake[i].Location.X;
+                y1 = snake[i].Location.Y;
+
+                PictureBox p = panel1.Controls["pct" + 0] as PictureBox;
+
+                x = p.Location.X;
+                y = p.Location.Y;
+                p.Location = new Point(pictureBox1.Location.X, pictureBox1.Location.Y);
+
+
+
+
+            }
+
+            switch (loc)
+            {
+                case 1:
+                    pictureBox1.Location = new Point(pictureBox1.Location.X + 25, pictureBox1.Location.Y );
+                    fx = -25;
+                    fy = 0;
+                    break;
+                case 2:
+                    pictureBox1.Location = new Point(pictureBox1.Location.X - 25, pictureBox1.Location.Y);
+                    fx = 25;
+                    fy = 0;
+                    break;
+                case 3:
+                    pictureBox1.Location = new Point(pictureBox1.Location.X, pictureBox1.Location.Y - 25);
+                    fy = 25;
+                    fx = 0;
+                    break;
+                case 4:
+                    pictureBox1.Location = new Point(pictureBox1.Location.X, pictureBox1.Location.Y + 25);
+                    fy = -25;
+                    fx = 0;
+                    break;
+
+            }
+           
+        }
+        
+    }
+}
